4c99534c1ca2b0c263c0a0f8c15591c86087f94e
Prakash-1619 <poojariprakash88@gmail.com>
2025-05-26 16:02:06 +0530

    importing plotly go

--- new_app.py
+++ new_app.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
 
 # --- Streamlit App Title ---
