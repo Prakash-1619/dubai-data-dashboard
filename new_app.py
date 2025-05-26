e9485bb73db52b54673c3117c1a398bb5cecc837
Prakash-1619 <poojariprakash88@gmail.com>
2025-05-26 19:58:39 +0530

    updated library

--- new_app.py
+++ new_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 
 # --- Page Config ---
 st.set_page_config(layout="wide")
