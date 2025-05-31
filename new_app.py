323b0877842021c9b345ab4c034c11c086d48db7
Prakash-1619 <poojariprakash88@gmail.com>
2025-05-31 13:37:28 +0530

    added ""

--- new_app.py
+++ new_app.py
@@ -405,7 +405,7 @@ if sidebar_option == "Price Prediction Model":
             st.warning(f"Comparison HTML not found at: {html_comparision}")
 
         st.subheader("📊 Logistic Regression")
-        st.markdown("###Equation : Predicted_price = 0.40134 * Actual_price + 8966.97)
+        st.markdown("###Equation : Predicted_price = 0.40134 * Actual_price + 8966.97")
         if os.path.exists(html_lr):
             with open(html_lr, "r", encoding="utf-8") as f:
                 components.html(f.read(), height=400, scrolling=True)
@@ -413,7 +413,7 @@ if sidebar_option == "Price Prediction Model":
             st.warning(f"Logistic Regression HTML not found at: {html_lr}")
 
         st.subheader("🌳 Decision Tree")
-        st.markdown("###Equation : Predicted_price = 0.465166 * Actual_price + 7993.22)
+        st.markdown("###Equation : Predicted_price = 0.465166 * Actual_price + 7993.22")
         if os.path.exists(html_dt):
             with open(html_dt, "r", encoding="utf-8") as f:
                 components.html(f.read(), height=400, scrolling=True)
@@ -421,7 +421,7 @@ if sidebar_option == "Price Prediction Model":
             st.warning(f"Decision Tree HTML not found at: {html_dt}")
 
         st.subheader("🚀 XGBoost")
-        st.markdown("###Equation : Predicted_price = 0.463650 * Actual_price + 8055.86)
+        st.markdown("###Equation : Predicted_price = 0.463650 * Actual_price + 8055.86")
         if os.path.exists(html_xgb):
             with open(html_xgb, "r", encoding="utf-8") as f:
                 components.html(f.read(), height=400, scrolling=True)
